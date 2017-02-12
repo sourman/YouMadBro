434a6631289ed2991ce6872ef9fc72878cffee2e
JimmmerS_Mac <jws6720@rit.edu>
2017-02-12 15:26:49 -0500

    I remove a lot of the useless code from the MainActivity.java file.
    Everything still needs to be commented.

--- app/src/main/java/com/example/jimmmers/youmadbro/MainActivity.java
+++ app/src/main/java/com/example/jimmmers/youmadbro/MainActivity.java
@@ -34,42 +34,22 @@ import java.util.Locale;
 import java.util.Map;
 
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
+
 public class MainActivity extends Activity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
+
     private static final boolean AUTO_HIDE = true;
 
-
-    //#$#$#$#$#$#$#$
-
-
     String[] emotions = {"Sadness","Fear","Anger","Disgust","Joy"};
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
+
     private SpeechRecognizer mSpeechRecognizer;
     private Intent mSpeechRecognizerIntent;
 
     private boolean mIslistening;
 
     private static final int AUTO_HIDE_DELAY_MILLIS = 1000;
-    //private SpeechRecognizer mSpeechRecognizer;
-    private Intent i;
-    //private boolean mIsListening;
 
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
+    private Intent i;
+
     private static final boolean TOGGLE_ON_CLICK = true;
 
     /**
@@ -91,6 +71,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         ready = false;
 
+        //This is some of the code for the splash screen that needs to be finished
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.splash);
         ImageView splash = (ImageView)findViewById(R.id.splash);
@@ -98,22 +79,11 @@ public class MainActivity extends Activity {
         Drawable mad = res2.getDrawable(R.drawable.umadbro);
         splash.setImageDrawable(mad);
 
-
-//        for(int i = -100000; i < 100000; i++){
-//            System.out.println();
-//        }
-//        try{
-//            Thread.sleep(3000);
-//        }catch(InterruptedException e){
-//            e.printStackTrace();
-//        }
-
         setContentView(R.layout.activity_main);
-
-        //final View controlsView = findViewById(R.id.fullscreen_content_controls);
         final View contentView = findViewById(R.id.fullscreen_content);
 
 
+        //This code sets up the google speech prompt
         mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
         mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
@@ -122,8 +92,8 @@ public class MainActivity extends Activity {
                 this.getPackageName());
 
 
-        SpeechRecognitionListener listener = new SpeechRecognitionListener();
-        mSpeechRecognizer.setRecognitionListener(listener);
+        //SpeechRecognitionListener listener = new SpeechRecognitionListener();
+        //mSpeechRecognizer.setRecognitionListener(listener);
 
         mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
         mSystemUiHider.setup();
@@ -137,10 +107,7 @@ public class MainActivity extends Activity {
                     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
                     public void onVisibilityChange(boolean visible) {
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-                            // If the ViewPropertyAnimator API is available
-                            // (Honeycomb MR2 and later), use it to animate the
-                            // in-layout UI controls at the bottom of the
-                            // screen.
+
                             if (mControlsHeight == 0) {
                                 //mControlsHeight = controlsView.getHeight();
                             }
@@ -148,14 +115,7 @@ public class MainActivity extends Activity {
                                 mShortAnimTime = getResources().getInteger(
                                         android.R.integer.config_shortAnimTime);
                             }
-                            //controlsView.animate()
-                            //.translationY(visible ? 0 : mControlsHeight)
-                            //.setDuration(mShortAnimTime);
-                        } else {
-                            // If the ViewPropertyAnimator APIs aren't
-                            // available, simply show or hide the in-layout UI
-                            // controls.
-                            //controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
+
                         }
 
                         if (visible && AUTO_HIDE) {
@@ -169,28 +129,14 @@ public class MainActivity extends Activity {
         contentView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                if (!mIsListening) {
-//                    mSpeechRecognizer.startListening(i);
-//                }
                 promptSpeechInput();
-
-
             }
         });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
         delayedHide(0);
     }
 
@@ -236,24 +182,6 @@ public class MainActivity extends Activity {
     }
 
 
-
-    public void promptSpeechInput2(){
-        /*i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        i.putExtra(RecognizerIntent.EXTRA_PROMPT,"Say something");*/
-
-        mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
-
-        try {
-            startActivityForResult(mSpeechRecognizerIntent,100);
-
-        }
-        catch(ActivityNotFoundException e){
-            Toast.makeText(MainActivity.this, "Sorry, no", Toast.LENGTH_SHORT).show();
-        }
-    }
-
     public void onActivityResult(int request_code, int result_code, Intent i){
         super.onActivityResult(request_code,result_code,i);
         switch(request_code){
@@ -264,47 +192,7 @@ public class MainActivity extends Activity {
                 resultText = result.get(0);
                 TextView text = (TextView) findViewById(R.id.fullscreen_content);
                 text.setText(resultText);
-                /*
-                int count = 0;
 
-                for(int v=0;v<mood.length;v++){
-                    count++;
-
-                    if(resultText.contains(mood[v])){
-                        if(v<20){
-                            setEmotion("Sadness");
-                            break;
-                        }
-                        else if(v>=20&&v<40){
-                            setEmotion("Fear");
-                            break;
-                        }
-                        else if(v>=40&&v<60){
-                            setEmotion("Anger");
-                            break;
-                        }
-                        else if(v>=60&&v<80){
-                            setEmotion("Disgust");
-                            break;
-                        }else {
-                            setEmotion("Joy");
-                            break;
-                        }
-                    }
-                }
-                if(count>=100){
-                    setEmotion("Neutral");
-                }*/
-                //promptSpeechInput();
-                //appDev b = new appDev();
-                //String emo = appDev.biggest(resultText);
-                //setEmotion(MainActivity.biggest(resultText));
-                //AskWatsonTask task = new AskWatsonTask();
-                //tring answer1 = "";
-                //answer1 = task.doInBackground(resultText);
-                //setEmotion(task.doInBackground(resultText));
-                //task.execute(new String[]{});
-                //setEmotion("Joy");
                 MyThread thread = new MyThread(resultText);
                 thread.start();
                 try {
@@ -312,7 +200,6 @@ public class MainActivity extends Activity {
                 }catch(InterruptedException j){
                     j.printStackTrace();
                 }
-                //setEmotion(thread.getTheTone());
 
                 String toneSen = thread.getTheTone().toString();
                 String[] split = toneSen.split("\\s");
@@ -338,13 +225,9 @@ public class MainActivity extends Activity {
 
                 }
                 setEmotion(emotions[index]);
-
             }
                 break;
         }
-
-
-
     }
 
     private void setEmotion(String emotion){
@@ -392,105 +275,8 @@ public class MainActivity extends Activity {
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 
-    public static String biggest(final String text){
-//		ToneAnalyzer service = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_05_19);
-//
-//		service.setUsernameAndPassword("1079029d-f17e-4bd0-800d-a7340ead306b", "wrM841ZlRaHA");
-//
-        MyThread thread = new MyThread(text);
-        ToneAnalysis tone = thread.getTheTone();
-
-        String toneSen = tone.toString();
-        String[] split = toneSen.split("\\s");
-        float[] numberArray = new float[5];
-        String[] emotions = {"Anger","Disgust","Fear", "Joy", "Sadness"};
-        int count=0;
-        for(int i=0;i<split.length;i++){
-            if(count == 5){
-                break;
-            }
-            split[i].trim();
-            if(split[i].length()!=0&&split[i].charAt(0)=='0'){
-                numberArray[count++] =  Float.valueOf(split[i]);
-            }
-        }
-        float max =numberArray[0];
-        int index =0;
-        for(int i=0;i<numberArray.length;i++){
-            if(max<=numberArray[i]){
-                max = numberArray[i];
-                index =i;
-            }
-
-        }
-
-
-        return emotions[index];
-    }
-
-    private class AskWatsonTask extends AsyncTask<String, Void, String> {
-        String answer = "";
-        ToneAnalysis anyTone;
-
-        @Override
-        protected String doInBackground(String... texts){
-            final String text = texts[0];
-
-
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    ToneAnalyzer service = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_05_19);
-                    //service.setUsernameAndPassword("1079029d-f17e-4bd0-800d-a7340ead306b", "wrM841ZlRaHA");
-                    service.setUsernameAndPassword("b361e424-749a-4ed5-88d5-825655fd556e", "wQ00N2kq7G68");
-                    anyTone = service.getTone(text, null).execute();
-
-                }
-            }).start();
-
-
-//            ToneAnalyzer service = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_05_19);
-//            service.setUsernameAndPassword("1079029d-f17e-4bd0-800d-a7340ead306b", "wrM841ZlRaHA");
-//            ToneAnalysis tone = service.getTone(resultText, null).execute();
-            //MainActivity.MyThread thread = new MyThread(resultText);
-            //thread.start();
-            //ToneAnalysis tone = thread.getTheTone();
-
-            String toneSen = anyTone.toString();
-            String[] split = toneSen.split("\\s");
-            float[] numberArray = new float[5];
-            String[] emotions = {"Anger","Disgust","Fear", "Joy", "Sadness"};
-            int count=0;
-            for(int i=0;i<split.length;i++){
-                if(count == 5){
-                    break;
-                }
-                split[i].trim();
-                if(split[i].length()!=0&&split[i].charAt(0)=='0'){
-                    numberArray[count++] =  Float.valueOf(split[i]);
-                }
-            }
-            float max =numberArray[0];
-            int index =0;
-            for(int i=0;i<numberArray.length;i++){
-                if(max<=numberArray[i]){
-                    max = numberArray[i];
-                    index =i;
-                }
-
-            }
-            ImageView emoji = (ImageView)findViewById(R.id.emoji);
-            Resources res = getResources();
-            //setEmotion(emotions[index], emoji, res);
-            return emotions[index];
-        }
-
-        @Override
-        protected void onPostExecute(String result){
-
-        }
-    }
-
+    
+    //The network operation must be rum on a seperate thread
     public static class MyThread extends Thread {
         String text;
         ToneAnalysis tone;
@@ -514,94 +300,5 @@ public class MainActivity extends Activity {
             return tone;
         }
     }
-
-    protected class SpeechRecognitionListener implements RecognitionListener
-    {
-
-        @Override
-        public void onBeginningOfSpeech()
-        {
-            //Log.d(TAG, "onBeginingOfSpeech");
-        }
-
-        @Override
-        public void onBufferReceived(byte[] buffer)
-        {
-
-        }
-
-        @Override
-        public void onEndOfSpeech()
-        {
-            //Log.d(TAG, "onEndOfSpeech");
-        }
-
-        @Override
-        public void onError(int error)
-        {
-            mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
-
-            //Log.d(TAG, "error = " + error);
-        }
-
-        @Override
-        public void onEvent(int eventType, Bundle params)
-        {
-
-        }
-
-        @Override
-        public void onPartialResults(Bundle partialResults)
-        {
-
-        }
-
-        @Override
-        public void onReadyForSpeech(Bundle params)
-        {
-            Log.d(TAG, "onReadyForSpeech"); //$NON-NLS-1$
-        }
-
-        @Override
-        public void onResults(Bundle results)
-        {
-            //Log.d(TAG, "onResults"); //$NON-NLS-1$
-            ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-            // matches are the return values of speech recognition engine
-            // Use these values for whatever you wish to do
-        }
-
-        @Override
-        public void onRmsChanged(float rmsdB)
-        {
-        }
-    }
-
-    private class ReadyThread extends Thread{
-        int wait = 2000;
-        boolean rdy;
-
-        public ReadyThread(){
-            rdy = false;
-        }
-
-        public void run(){
-            if(wait <= 0){
-                rdy = true;
-            }else {
-                wait -= 100;
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        public boolean isRdy(){
-            return rdy;
-        }
-    }
-
 
 }
